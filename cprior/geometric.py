d04d4748cd0aec116a98b5d17861172ee7797760
GUI <g.navas.palencia@gmail.com>
2019-05-12 12:14:22 +0200

    Implement geometric model with posterior predictive distribution, mean and variance

--- cprior/geometric.py
+++ cprior/geometric.py
@@ -7,6 +7,8 @@ Bayesian model with geometric likelihood.
 
 import numpy as np
 
+from scipy import special
+
 from .cdist import BetaABTest
 from .cdist import BetaModel
 from .cdist.utils import check_models
@@ -15,6 +17,17 @@ from .cdist.utils import check_models
 class GeometricModel(BetaModel):
     """
     Bayesian model with geometric likelihood and a beta prior distribution.
+
+    Given data samples :math:`\mathbf{x} = (x_1, \ldots, x_n)`
+    from a geometric distribution with parameter :math:`p`, the posterior
+    distribution is
+
+    .. math::
+
+        p | \\mathbf{x} \\sim \\mathcal{B}\\left(\\alpha + n, \\beta +
+        \\sum_{i=1}^n x_i - n \\right),
+
+    with prior parameters :math:`\\alpha` and :math:`\\beta`.
 
     Parameters
     ----------
@@ -39,6 +52,93 @@ class GeometricModel(BetaModel):
         n = len(data)
         self._alpha_posterior += n
         self._beta_posterior += np.sum(data) - n
+
+    def pppdf(self, x):
+        """
+        Posterior predictive probability density function.
+
+        If :math:`X` follows a geometric distribution with parameter
+        :math:`p \\sim \\mathcal{B}(\\alpha, \\beta)`, then the posterior
+        predictive probability density function is given by
+
+        .. math::
+
+            f(x; \\alpha, \\beta) = \\frac{B(\\alpha + 1, \\beta + x - 1)}{B(
+            \\alpha + \\beta)},
+
+        where :math:`\\alpha` and :math:`\\beta` are the posterior values
+        of the parameters.
+
+        Parameters
+        ----------
+        x : array-like
+            Quantiles.
+
+        Returns
+        -------
+        pdf : float
+            Probability density function evaluated at x.
+        """
+        a = self._alpha_posterior
+        b = self._beta_posterior
+
+        logpdf = special.betaln(a + 1, b + x - 1) - special.betaln(a, b)
+        return np.exp(logpdf)
+
+    def ppmean(self):
+        """
+        Posterior predictive mean.
+
+        If :math:`X` follows a geometric distribution with parameter
+        :math:`\\lambda`, then the posterior predictive expected value is given
+        by
+
+        .. math::
+
+            \\mathrm{E}[X] = \\frac{\\alpha + \\beta - 1}{\\alpha - 1},
+
+        where :math:`\\alpha` and :math:`\\beta` are the posterior values
+        of the parameters.
+
+        Returns
+        -------
+        mean : float
+        """
+        a = self._alpha_posterior
+        b = self._beta_posterior
+
+        if a > 1:
+            return (a + b - 1) / (a - 1)
+        else:
+            return np.nan
+
+    def ppvar(self):
+        """
+        Posterior predictive variance.
+
+        If :math:`X` follows a geometric distribution with parameter
+        :math:`p \\sim \\mathcal{B}(\\alpha, \\beta)`, then the posterior
+        predictive variance is given by
+
+        .. math::
+
+            \\mathrm{Var}[X] = \\frac{\\alpha \\beta (\\alpha + \\beta - 1)}{
+            (\\alpha - 1)^2 (\\alpha - 2)},
+
+        where :math:`\\alpha` and :math:`\\beta` are the posterior values
+        of the parameters.
+
+        Returns
+        -------
+        var : float
+        """
+        a = self._alpha_posterior
+        b = self._beta_posterior
+
+        if a > 2:
+            return a * b * (a + b - 1) / ((a - 1) ** 2 * (a - 2))
+        else:
+            return np.nan
 
 
 class GeometricABTest(BetaABTest):

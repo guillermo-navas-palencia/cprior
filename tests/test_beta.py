daf45adf38245cf17681b0a3da934ff3c3c0386b
GUI <g.navas.palencia@gmail.com>
2019-05-23 01:28:47 +0200

    Add beta_cprior basic testing

--- tests/test_beta.py
+++ tests/test_beta.py
@@ -5,25 +5,46 @@ Beta distribution testing
 # Guillermo Navas-Palencia <g.navas.palencia@gmail.com>
 # Copyright (C) 2019
 
-from pytest import approx
+from pytest import approx, raises
 
 from cprior._lib.cprior import beta_cprior
+from cprior.cdist import BetaModel
 
 
 def test_beta_small_a0():
-    beta_cprior(10, 50, 60, 350) == approx(0.7088919200702957, rel=1e-8)
+    assert beta_cprior(10, 50, 60, 250) == approx(0.7088919200, rel=1e-8)
+
 
 def test_beta_small_a1():
-    beta_cprior(600, 550, 52, 60) == approx(0.12231442584597124, rel=1e-8)
+    assert beta_cprior(600, 550, 52, 60) == approx(0.1223144258, rel=1e-8)
+
 
 def test_beta_small_b0():
-    beta_cprior(1000, 900, 1200, 1000) == approx(0.8898254504596144, rel=1e-8)
+    assert beta_cprior(1000, 900, 1200, 1000) == approx(0.8898254504, rel=1e-8)
+
 
 def test_beta_small_b1():
-    beta_cprior(1000, 900, 1200, 800) == approx(0.999998265666369, rel=1e-8)
+    assert beta_cprior(1000, 900, 1200, 800) == approx(0.9999982656, rel=1e-8)
+
 
 def test_beta_equal_params_integer():
-    beta_cprior(100, 90, 100, 90) == approx(0.5, rel=1e-8)
+    assert beta_cprior(100, 90, 100, 90) == approx(0.5)
+
 
 def test_beta_equal_params_float():
-    beta_cprior(100.1, 90.1, 100.1, 90.1) == approx(0.5, rel=1e-8)
+    assert beta_cprior(100.1, 90.1, 100.1, 90.1) == approx(0.5)
+
+
+def test_beta_model_alpha_positive():
+    with raises(ValueError):
+        model = BetaModel(alpha=-1)
+
+
+def test_beta_model_beta_positive():
+    with raises(ValueError):
+        model = BetaModel(beta=0)
+
+
+def test_beta_model_mean():
+    model = BetaModel(alpha=4, beta=6)
+    assert model.mean() == approx(0.4)

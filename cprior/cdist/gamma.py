588499f09966e69b53d8dc7369870222748854b2
GUI <g.navas.palencia@gmail.com>
2019-06-21 22:45:26 +0200

    Implement MV methods and tests

--- cprior/cdist/gamma.py
+++ cprior/cdist/gamma.py
@@ -7,18 +7,29 @@ Gamma conjugate prior distribution model.
 
 import numpy as np
 
+from multiprocessing import Pool
 from scipy import optimize
 from scipy import special
 from scipy import stats
 
 from .base import BayesABTest
 from .base import BayesModel
+from .base import BayesMVTest
 from .utils import check_ab_method
+from .utils import check_mv_method
 
 
 def func_ppf(x, a0, b0, a1, b1, p):
     """Function CDF ratio of gamma function for root-finding."""
     return special.betainc(a0, a1, x / (x + b1 / b0)) - p
+
+
+def func_mv_ppf(x, variant_params, p):
+    """Function CDF of max of gamma random variables for root-finding."""
+    cdf = 1.0
+    for (a, b) in variant_params:
+        cdf *= special.gammainc(a, b * x)
+    return cdf - p
 
 
 class GammaModel(BayesModel):
@@ -503,3 +514,399 @@ class GammaABTest(BayesABTest):
                     variant="A", interval_length=interval_length),
                     self.expected_loss_relative_ci(method=method,
                     variant="B", interval_length=interval_length))                
+
+
+class GammaMVTest(BayesMVTest):
+    """
+    Bayesian Multivariate testing with prior gamma distribution.
+
+    Parameters
+    ----------
+    models : object
+        The gamma models.
+
+    simulations : int or None (default=1000000)
+        Number of Monte Carlo simulations.
+
+    random_state : int or None (default=None)
+        The seed used by the random number generator.
+    """
+    def __init__(self, models, simulations=None, random_state=None,
+        n_jobs=None):
+        super().__init__(models, simulations, random_state, n_jobs)
+
+    def probability(self, method="exact", control="A", variant="B", lift=0):
+        """
+        Compute the error probability or *chance to beat control*, i.e.,
+        :math:`P[variant > control + lift]`.
+
+        If ``lift`` is positive value, the computation method must be Monte
+        Carlo sampling.
+
+        Parameters
+        ----------
+        method : str (default="exact")
+            The method of computation. Options are "exact", "MC" (Monte Carlo)
+            and "MLHS" (Monte Carlo + Median Latin Hypercube Sampling).
+
+        control : str (default="A")
+            The control variant.
+
+        variant : str (default="B")
+            The tested variant.
+
+        lift : float (default=0.0)
+           The amount of uplift.
+        """
+        check_mv_method(method=method, method_options=("exact", "MC"),
+            control=control, variant=variant, variants=self.models.keys(),
+            lift=lift)
+
+        model_control = self.models[control]
+        model_variant = self.models[variant]
+
+        if method == "exact":
+            a0 = model_control.shape_posterior
+            b0 = model_control.rate_posterior
+
+            a1 = model_variant.shape_posterior
+            b1 = model_variant.rate_posterior
+
+            p = b0 / (b0 + b1)
+            return special.betainc(a0, a1, p)
+        else:
+            x0 = model_control.rvs(self.simulations, self.random_state)
+            x1 = model_variant.rvs(self.simulations, self.random_state)
+
+            return (x1 > x0 + lift).mean()
+
+    def probability_vs_all(self, method="MLHS", variant="B", lift=0,
+        mlhs_samples=1000):
+        r"""
+        Compute the error probability or *chance to beat all* variations. For
+        example, given variants "A", "B", "C" and "D", and choosing variant="B",
+        we compute :math:`P[B > \max{A, C, D} + lift]`.
+
+        If ``lift`` is positive value, the computation method must be Monte
+        Carlo sampling.
+
+        Parameters
+        ----------
+        method : str (default="exact")
+            The method of computation. Options are "MC" (Monte Carlo)
+            and "MLHS" (Monte Carlo + Median Latin Hypercube Sampling).
+
+        variant : str (default="B")
+            The chosen variant.
+
+        lift : float (default=0.0)
+           The amount of uplift.
+
+        mlhs_samples : int (default=1000)
+            Number of samples for MLHS method.
+        """
+        check_mv_method(method=method, method_options=("MC", "MLHS"),
+            control=None, variant=variant, variants=self.models.keys(),
+            lift=lift)
+
+        # exclude variant
+        variants = list(self.models.keys())
+        variants.remove(variant)
+
+        if method == "MC":
+            # generate samples from all models in parallel
+            xvariant = self.models[variant].rvs(self.simulations,
+                self.random_state)
+
+            pool = Pool(processes=self.n_jobs)
+            processes = [pool.apply_async(self._rvs, args=(v, ))
+                for v in variants]
+            xall = [p.get() for p in processes]
+            maxall = np.maximum.reduce(xall)
+
+            return (xvariant > maxall + lift).mean()
+        else:
+            # prepare parameters
+            variant_params = [(self.models[v].shape_posterior,
+                self.models[v].rate_posterior) for v in variants]
+
+            r = np.arange(mlhs_samples)
+            np.random.shuffle(r)
+            v = (r - 0.5) / mlhs_samples
+            v = v[v >= 0]
+            x = self.models[variant].ppf(v)
+
+            return np.nanmean(np.prod([special.gammainc(a, b * x)
+                for a, b in variant_params], axis=0))
+
+    def expected_loss(self, method="exact", control="A", variant="B", lift=0):
+        r"""
+        Compute the expected loss. This is the expected uplift lost by choosing
+        a given variant, i.e., :math:`\mathrm{E}[\max(control - variant -
+        lift, 0)]`.
+
+        If ``lift`` is positive value, the computation method must be Monte
+        Carlo sampling.
+
+        Parameters
+        ----------
+        method : str (default="exact")
+            The method of computation. Options are "exact" and "MC".
+
+        control : str (default="A")
+            The control variant.
+
+        variant : str (default="B")
+            The tested variant.
+
+        lift : float (default=0.0)
+           The amount of uplift.
+        """
+        check_mv_method(method=method, method_options=("exact", "MC"),
+            control=control, variant=variant, variants=self.models.keys(),
+            lift=lift)
+
+        model_control = self.models[control]
+        model_variant = self.models[variant]
+
+        if method == "exact":
+            a0 = model_control.shape_posterior
+            b0 = model_control.rate_posterior
+
+            a1 = model_variant.shape_posterior
+            b1 = model_variant.rate_posterior
+
+            t0 = a0 / b0 * special.betainc(a1, a0 + 1, b1 / (b0 + b1))
+            t1 = a1 / b1 * special.betainc(a1 + 1, a0, b1 / (b0 + b1))
+            return t0 - t1
+        else:
+            x0 = model_control.rvs(self.simulations, self.random_state)
+            x1 = model_variant.rvs(self.simulations, self.random_state)
+
+            return np.maximum(x0 - x1, 0).mean()
+
+    def expected_loss_ci(self, method="MC", control="A", variant="B",
+        interval_length=0.9):
+        """
+        Compute credible intervals on the difference distribution of
+        :math:`Z = control-variant`.
+
+        Parameters
+        ----------
+        method : str (default="MC")
+            The method of computation. Options are "asymptotic" and "MC".
+
+        control : str (default="A")
+            The control variant.
+
+        variant : str (default="B")
+            The tested variant.
+
+        interval_length : float (default=0.9)
+            Compute ``interval_length``\% credible interval. This is a value in
+            [0, 1].
+        """
+        check_mv_method(method=method, method_options=("MC", "asymptotic"),
+            control=control, variant=variant, variants=self.models.keys(),
+            interval_length=interval_length)
+
+        # check interval length
+        lower = (1 - interval_length) / 2
+        upper = (1 + interval_length) / 2
+
+        model_control = self.models[control]
+        model_variant = self.models[variant]
+
+        if method == "MC":
+            x0 = model_control.rvs(self.simulations, self.random_state)
+            x1 = model_variant.rvs(self.simulations, self.random_state)
+
+            lower *= 100.0
+            upper *= 100.0
+
+            return np.percentile((x0 - x1), [lower, upper])
+        else:
+            a0 = model_control.shape_posterior
+            b0 = model_control.rate_posterior
+
+            a1 = model_variant.shape_posterior
+            b1 = model_variant.rate_posterior
+
+            mu = a1 / b1 - a0 / b0
+            var0 = a0 / b0 ** 2
+            var1 = a1 / b1 ** 2
+            sigma = np.sqrt(var0 + var1)
+
+            return stats.norm(-mu, sigma).ppf([lower, upper])
+
+    def expected_loss_relative(self, method="exact", control="A", variant="B"):
+        r"""
+        Compute expected relative loss for choosing a variant. This can be seen
+        as the negative expected relative improvement or uplift, i.e.,
+        :math:`\mathrm{E}[(control - variant) / variant]`.
+
+        Parameters
+        ----------
+        method : str (default="exact")
+            The method of computation. Options are "exact" and "MC".
+
+        control : str (default="A")
+            The control variant.
+
+        variant : str (default="B")
+            The tested variant.
+        """
+        check_mv_method(method=method, method_options=("exact", "MC"),
+            control=control, variant=variant, variants=self.models.keys())
+
+        model_control = self.models[control]
+        model_variant = self.models[variant]
+
+        if method == "exact":
+            a0 = model_control.shape_posterior
+            b0 = model_control.rate_posterior
+
+            a1 = model_variant.shape_posterior
+            b1 = model_variant.rate_posterior
+
+            return b1 / b0 * a0 / (a1 - 1) - 1
+        else:
+            x0 = model_control.rvs(self.simulations, self.random_state)
+            x1 = model_variant.rvs(self.simulations, self.random_state)
+
+            return ((x0 - x1) / x1).mean()
+
+    def expected_loss_relative_ci(self, method="MC", control="A", variant="B",
+        interval_length=0.9):
+        """
+        Compute credible intervals on the relative difference distribution of
+        :math:`Z = (control - variant) / variant`.
+
+        Parameters
+        ----------
+        method : str (default="MC")
+            The method of computation. Options are "asymptotic", "exact" and
+            "MC".
+
+        control : str (default="A")
+            The control variant.
+
+        variant : str (default="B")
+            The tested variant.
+
+        interval_length : float (default=0.9)
+            Compute ``interval_length``\% credible interval. This is a value in
+            [0, 1].
+        """
+        check_mv_method(method=method, method_options=("asymptotic", "exact",
+            "MC"), control=control, variant=variant,
+            variants=self.models.keys(), interval_length=interval_length)
+
+        lower = (1 - interval_length) / 2
+        upper = (1 + interval_length) / 2
+
+        model_control = self.models[control]
+        model_variant = self.models[variant]
+
+        if method == "MC":
+            x0 = model_control.rvs(self.simulations, self.random_state)
+            x1 = model_variant.rvs(self.simulations, self.random_state)
+
+            lower *= 100.0
+            upper *= 100.0
+
+            return np.percentile((x0 - x1) / x1, [lower, upper])
+        else:
+            a0 = model_control.shape_posterior
+            b0 = model_control.rate_posterior
+
+            a1 = model_variant.shape_posterior
+            b1 = model_variant.rate_posterior
+
+            mu = b1 / b0 * a0 / (a1 - 1)
+            var = a0 * (a0 + a1 - 1) / (a1 - 2) / (a1 - 1)**2
+            var *= (b1 / b0) ** 2
+            sigma = np.sqrt(var)
+
+            dist = stats.norm(mu, sigma)
+            ppfl, ppfu = dist.ppf([lower, upper])
+
+            if method == "asymptotic":
+                return ppfl - 1, ppfu - 1
+            else:
+                ppfl = optimize.newton(func=func_ppf, x0=ppfl,
+                    args=(a0, b0, a1, b1, lower), maxiter=1000)
+
+                ppfu = optimize.newton(func=func_ppf, x0=ppfu,
+                    args=(a0, b0, a1, b1, upper), maxiter=1000)
+
+                return ppfl - 1, ppfu - 1
+
+    def expected_loss_vs_all(self, method="MC", variant="B", lift=0,
+        mlhs_samples=1000):
+        r"""
+        Compute the expected loss against all variations. For example, given
+        variants "A", "B", "C" and "D", and choosing variant="B", we compute
+        :math:`\mathrm{E}[\max(\max(A, B, C) - B, 0)]`.
+
+        If ``lift`` is positive value, the computation method must be Monte
+        Carlo sampling.
+
+        Parameters
+        ----------
+        method : str (default="exact")
+            The method of computation. Options are "exact", "MC" (Monte Carlo)
+            and "MLHS" (Monte Carlo + Median Latin Hypercube Sampling).
+
+        variant : str (default="B")
+            The chosen variant.
+
+        lift : float (default=0.0)
+           The amount of uplift.
+
+        mlhs_samples : int (default=1000)
+            Number of samples for MLHS method.
+        """
+        check_mv_method(method=method, method_options=("MC", "MLHS"),
+            control=None, variant=variant, variants=self.models.keys(),
+            lift=lift)
+
+        # exclude variant
+        variants = list(self.models.keys())
+        variants.remove(variant)
+
+        if method == "MC":
+            # generate samples from all models in parallel
+            xvariant = self.models[variant].rvs(self.simulations,
+                self.random_state)
+
+            pool = Pool(processes=self.n_jobs)
+            processes = [pool.apply_async(self._rvs, args=(v, ))
+                for v in variants]
+            xall = [p.get() for p in processes]
+            maxall = np.maximum.reduce(xall)
+
+            return np.maximum(maxall - xvariant - lift, 0).mean()
+        else:
+            r = np.arange(mlhs_samples)
+            np.random.shuffle(r)
+            v = (r - 0.5) / mlhs_samples
+            v = v[v >= 0]
+
+            # ppf of distribution of max(x0, x1, ..., xn), where x_i follows
+            # a beta distribution
+            variant_params = [(self.models[v].shape_posterior,
+                self.models[v].rate_posterior) for v in variants]
+
+            # TODO: improve this
+            maxb = np.max(self.models[variant].ppf(0.99999999))
+
+            x = np.array([optimize.brentq(f=func_mv_ppf,
+                args=(variant_params, p), a=0, b=maxb, xtol=1e-4, rtol=1e-4
+                ) for p in v])
+
+            a = self.models[variant].shape_posterior
+            b = self.models[variant].rate_posterior
+            p = x * special.gammainc(a, b * x)
+            q = a /  b * special.gammainc(a + 1, b * x)
+            return np.nanmean(p - q)
--- tests/test_gamma.py
+++ tests/test_gamma.py
@@ -9,6 +9,7 @@ from pytest import approx, raises
 
 from cprior.cdist import GammaABTest
 from cprior.cdist import GammaModel
+from cprior.cdist import GammaMVTest
 
 
 def test_gamma_model_shape_positive():
@@ -181,3 +182,202 @@ def test_gamma_ab_expected_loss_relative_ci_large_params():
 
     assert abtest.expected_loss_relative_ci(method="asymptotic",
         variant="B") == approx((-0.2030159429, -0.1287506763), rel=1e-1)
+
+
+def test_gamma_mv_check_method():
+    models = {
+        "A": GammaModel(shape=25, rate=10000),
+        "B": GammaModel(shape=30, rate=10000)
+    }
+
+    mvtest = GammaMVTest(models)
+
+    with raises(ValueError):
+        mvtest.probability(method="new")
+
+
+def test_gamma_mv_probability():
+    modelA = GammaModel(shape=2500, rate=1000000)
+    modelB = GammaModel(shape=3000, rate=1000000)
+    abtest = GammaABTest(modelA, modelB, 1000000, 42)
+    mvtest = GammaMVTest({"A": modelA, "B": modelB}, 1000000, 42)
+
+    ab_result = abtest.probability(method="exact", variant="A")
+    mv_result = mvtest.probability(method="exact", control="B", variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-8)
+
+    ab_result = abtest.probability(method="MC", variant="A")
+    mv_result = mvtest.probability(method="MC", control="B", variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.probability(method="exact", variant="B")
+    mv_result = mvtest.probability(method="exact", variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-8)
+
+    ab_result = abtest.probability(method="MC", variant="B")
+    mv_result = mvtest.probability(method="MC", variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+
+def test_gamma_mv_probability_vs_all():
+    models = {
+        "A": GammaModel(shape=25, rate=10000),
+        "B": GammaModel(shape=30, rate=10000),
+        "C": GammaModel(shape=40, rate=11000)
+    }
+
+    mvtest = GammaMVTest(models, 1000000, 42)
+
+    assert mvtest.probability_vs_all(method="MLHS",
+        variant="B") == approx(0.1968051, rel=1e-2)
+
+    assert mvtest.probability_vs_all(method="MC",
+        variant="B") == approx(0.1968051, rel=1e-2)
+
+
+def test_gamma_mv_expected_loss():
+    modelA = GammaModel(shape=2500, rate=1000000)
+    modelB = GammaModel(shape=3000, rate=1000000)
+    abtest = GammaABTest(modelA, modelB, 1000000, 42)
+    mvtest = GammaMVTest({"A": modelA, "B": modelB}, 1000000, 42)
+
+    ab_result = abtest.expected_loss(method="exact", variant="A")
+    mv_result = mvtest.expected_loss(method="exact", control="B", variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-8)
+
+    ab_result = abtest.expected_loss(method="MC", variant="A")
+    mv_result = mvtest.expected_loss(method="MC", control="B", variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss(method="exact", variant="B")
+    mv_result = mvtest.expected_loss(method="exact", variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-8)
+
+    ab_result = abtest.expected_loss(method="MC", variant="B")
+    mv_result = mvtest.expected_loss(method="MC", variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+
+def test_gamma_mv_expected_loss_vs_all():
+    models = {
+        "A": GammaModel(shape=25, rate=10000),
+        "B": GammaModel(shape=30, rate=10000),
+        "C": GammaModel(shape=40, rate=11000)
+    }
+
+    mvtest = GammaMVTest(models, 1000000, 42)
+
+    assert mvtest.expected_loss_vs_all(method="MLHS",
+        variant="B") == approx(0.0007447680, rel=1e-2)
+
+    assert mvtest.expected_loss_vs_all(method="MC",
+        variant="B") == approx(0.0007447680, rel=1e-2)
+
+
+def test_gamma_mv_expected_loss_ci():
+    modelA = GammaModel(shape=2500, rate=1000000)
+    modelB = GammaModel(shape=3000, rate=1000000)
+    abtest = GammaABTest(modelA, modelB, 1000000, 42)
+    mvtest = GammaMVTest({"A": modelA, "B": modelB}, 1000000, 42)
+
+    ab_result = abtest.expected_loss_ci(method="MC", variant="A")
+    mv_result = mvtest.expected_loss_ci(method="MC", control="B",
+        variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_ci(method="asymptotic", variant="A")
+    mv_result = mvtest.expected_loss_ci(method="asymptotic", control="B",
+        variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_ci(method="MC", variant="B")
+    mv_result = mvtest.expected_loss_ci(method="MC", variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_ci(method="asymptotic", variant="B")
+    mv_result = mvtest.expected_loss_ci(method="asymptotic", variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+
+def test_gamma_mv_expected_loss_relative():
+    modelA = GammaModel(shape=2500, rate=1000000)
+    modelB = GammaModel(shape=3000, rate=1000000)
+    abtest = GammaABTest(modelA, modelB, 1000000, 42)
+    mvtest = GammaMVTest({"A": modelA, "B": modelB}, 1000000, 42)
+
+    ab_result = abtest.expected_loss_relative(method="exact", variant="A")
+    mv_result = mvtest.expected_loss_relative(method="exact", control="B",
+        variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_relative(method="MC", variant="A")
+    mv_result = mvtest.expected_loss_relative(method="MC", control="B",
+        variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_relative(method="exact", variant="B")
+    mv_result = mvtest.expected_loss_relative(method="exact", variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_relative(method="MC", variant="B")
+    mv_result = mvtest.expected_loss_relative(method="MC", variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+
+def test_gamma_mv_expected_loss_ci_relative():
+    modelA = GammaModel(shape=2500, rate=1000000)
+    modelB = GammaModel(shape=3000, rate=1000000)
+    abtest = GammaABTest(modelA, modelB, 1000000, 42)
+    mvtest = GammaMVTest({"A": modelA, "B": modelB}, 1000000, 42)
+
+    ab_result = abtest.expected_loss_relative_ci(method="exact", variant="A")
+    mv_result = mvtest.expected_loss_relative_ci(method="exact", control="B",
+        variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_relative_ci(method="MC", variant="A")
+    mv_result = mvtest.expected_loss_relative_ci(method="MC", control="B",
+        variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_relative_ci(method="asymptotic",
+        variant="A")
+    mv_result = mvtest.expected_loss_relative_ci(method="asymptotic",
+        control="B", variant="A")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_relative_ci(method="exact", variant="B")
+    mv_result = mvtest.expected_loss_relative_ci(method="exact", variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_relative_ci(method="MC", variant="B")
+    mv_result = mvtest.expected_loss_relative_ci(method="MC", variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-2)
+
+    ab_result = abtest.expected_loss_relative_ci(method="asymptotic",
+        variant="B")
+    mv_result = mvtest.expected_loss_relative_ci(method="asymptotic",
+        variant="B")
+
+    assert ab_result == approx(mv_result, rel=1e-2)

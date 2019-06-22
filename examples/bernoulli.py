cab4cb73d0ab3c85957cd6b646346fecab98c702
GUI <g.navas.palencia@gmail.com>
2019-06-22 17:22:39 +0200

    Simple bernoulli distribution example. Clean up

--- examples/bernoulli.py
+++ examples/bernoulli.py
@@ -1,17 +1,22 @@
+"""
+Example Bayesian model with Bernoulli distribution.
+"""
+
+# Guillermo Navas-Palencia <g.navas.palencia@gmail.com>
+# Copyright (C) 2019
+
 import scipy.stats as st
 
-from cprior.bernoulli import BernoulliModel
-from cprior.bernoulli import BernoulliABTest
+from cprior import BernoulliModel
+from cprior import BernoulliABTest
 
 # Two model variants A and B, and build A/B test
-
 modelA = BernoulliModel(alpha=1, beta=1)
 modelB = BernoulliModel(alpha=1, beta=1)
 
 test = BernoulliABTest(modelA=modelA, modelB=modelB, simulations=1000000)
 
 # Generate new data and update models
-
 data_A = st.bernoulli(p=0.10).rvs(size=1500, random_state=42)
 data_B = st.bernoulli(p=0.11).rvs(size=1600, random_state=42)
 

82bb0cf90767ee2e2d910207718d87c1ea9bcc10
GUI <g.navas.palencia@gmail.com>
2019-08-06 23:47:23 +0200

    Update doc version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Guillermo Navas-Palencia'
 author = 'Guillermo Navas-Palencia'
 
 # The short X.Y version
-version = '0.1.0'
+version = '0.2.0'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------

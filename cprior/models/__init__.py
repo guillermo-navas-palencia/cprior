7cf51b108969ffddd5c65cc58ec4d756452d9e6b
GUI <g.navas.palencia@gmail.com>
2019-08-28 00:20:01 +0200

    Missing file

---
+++ cprior/models/__init__.py
@@ -0,0 +1,27 @@
+from .bernoulli import BernoulliABTest
+from .bernoulli import BernoulliModel
+from .bernoulli import BernoulliMVTest
+
+from .binomial import BinomialABTest
+from .binomial import BinomialModel
+from .binomial import BinomialMVTest
+
+from .exponential import ExponentialABTest
+from .exponential import ExponentialModel
+from .exponential import ExponentialMVTest
+
+from .geometric import GeometricABTest
+from .geometric import GeometricModel
+from .geometric import GeometricMVTest
+
+from .negative_binomial import NegativeBinomialABTest
+from .negative_binomial import NegativeBinomialModel
+from .negative_binomial import NegativeBinomialMVTest
+
+from .poisson import PoissonABTest
+from .poisson import PoissonModel
+from .poisson import PoissonMVTest
+
+from .uniform import UniformABTest
+from .uniform import UniformModel
+from .uniform import UniformMVTest

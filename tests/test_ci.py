5ff555e7599707250753bd70d7de3a0377a85f31
GUI <g.navas.palencia@gmail.com>
2019-11-03 11:26:22 +0100

    Add tests for ci_interval_exact method

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -9,6 +9,7 @@ from pytest import approx, raises
 from scipy import stats
 
 from cprior.cdist.ci import ci_interval
+from cprior.cdist.ci import ci_interval_exact
 
 
 def test_ci_method():
@@ -36,3 +37,49 @@ def test_ci():
 
     low, high = ci_interval(x, interval_length=0.95, method="HDI")
     assert [-1.96, 1.96] == approx([low, high], rel=1e-2)
+
+
+def test_ci_exact_method():
+    dist = stats.norm
+
+    with raises(ValueError):
+        ci_interval_exact(dist, interval_length=0.9, method="new_method")
+
+
+def test_ci_exact_interval_length():
+    dist = stats.norm
+
+    with raises(ValueError):
+        ci_interval_exact(dist, interval_length=1.1, method="ETI")
+
+    with raises(ValueError):
+        ci_interval_exact(dist, interval_length=-0.1, method="ETI")
+
+
+def test_ci_exact_dist():
+    dist = None
+
+    with raises(TypeError):
+        ci_interval_exact(dist, interval_length=0.9, method="ETI")
+
+
+def test_ci_exact():
+    dist = stats.norm
+
+    low, high = ci_interval_exact(dist, interval_length=0.95, method="ETI")
+    assert [-1.959963984540054, 1.959963984540054] == approx(
+        [low, high], rel=1e-8)
+
+    low, high = ci_interval_exact(dist, interval_length=0.95, method="HDI")
+    assert [-1.959963984540054, 1.959963984540054] == approx(
+        [low, high], rel=1e-8)
+
+
+def test_ci_exact_hdi_bounds():
+    dist = stats.beta(4, 10)
+
+    bounds = [(0, 1), (0, 1)]
+    low, high = ci_interval_exact(dist, interval_length=0.95, method="HDI",
+                                  bounds=bounds)
+    assert [0.0744808884427984, 0.5138206804161226] == approx(
+        [low, high], rel=1e-8)

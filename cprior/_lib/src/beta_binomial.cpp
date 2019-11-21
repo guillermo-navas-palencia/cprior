9b0c29de14d5436ba9fe52a4d62ac211940ddcf8
GUI <g.navas.palencia@gmail.com>
2019-11-21 07:04:13 +0100

    Beta-binomial distribution CDF implementation in C++

---
+++ cprior/_lib/src/beta_binomial.cpp
@@ -0,0 +1,38 @@
+/*
+  Closed-form formulas for beta-binomial distribution.
+
+  Guillermo Navas-Palencia <g.navas.palencia@gmail.com>
+  Copyright (C) 2019
+*/
+
+#include <cmath>
+
+#include "cprior.hpp"
+
+double combln(int n, int k)
+{
+  return std::lgamma(n + 1) - std::lgamma(k + 1) - std::lgamma(n - k + 1); 
+}
+
+double beta_binomial_cdf_cprior(int k, int n, double a, double b)
+{
+  const double err = 1e-15;
+  const double bn = b + n;
+  const double a1 = a - 1;
+  const double n1 = n + 1;
+
+  double c, s, sp;
+
+  c = std::exp(combln(n, k) + betaln(a + k, b + n - k) - betaln(a, b));
+  s = c;
+  sp = s;
+  for (int j = k; j >= 0; j--) {
+    c *= j * (bn - j) / ((a1 + j) * (n1 - j));
+    s += c;
+    if (std::abs((s - sp) / s) < err)
+      break;
+    else
+      sp = s;
+  }
+  return s;
+}

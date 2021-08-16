713b8cf5086125a8cbd520fe3957509a694bebc1
AlonMenczer <alonm@spotnix.io>
2021-08-16 19:08:39 +0300

    Support mac os

--- cprior/_lib/build.sh
+++ cprior/_lib/build.sh
@@ -10,8 +10,14 @@ INCLUDE_DIR="$PROJECT_ROOT/include"
 g++ -c -O3 -Wall -fpic -march=native -std=c++11 $SOURCE_DIR/*.cpp \
   -I $INCLUDE_DIR
 
+
 # build library
-g++ -shared *.o -o _cprior.so
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  g++ -shared *.o -o _cprior.so
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  g++ -dynamiclib *.o -o cprior.dylib
+fi
 
 # move objects to build
 mkdir -p build
